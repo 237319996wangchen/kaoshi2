820b5d57494813fdad7d555f150cf658c3f17899
王晨 <wangchene@xiangha.com>
2020-03-15 20:14:18 +0800

    修改

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'class'       => 'yii\db\Connection',
+    'dsn'         => 'mysql:host=127.0.0.1:3306;dbname=yii2_basic_tests',
+    'username'    => 'root',
+    'password'    => '123456',
+    'charset'     => 'utf8',
+    'tablePrefix' => 'ks_',
+];
